785c85b847789f772534a1ef61b12cc09bdb0c10
kangaxx <lion_kangaxx@126.com>
2016-05-30 17:46:09 +0800

    proc no argument ok
    mysql function , by proc , no argument , is ok

--- HRGLinqLibrary/HRG_DBConnection.cs
+++ HRGLinqLibrary/HRG_DBConnection.cs
@@ -14,32 +14,63 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data.OleDb;
 
-namespace HRGLinqLibrary
+namespace HRG_LinqLibrary
 {
-    public enum HRG_SQLType
-    {
-        MYSQL = 1,
-        SQLSERVER2000 = 2,
-        SQLSERVER2005 = 3,
-        ORACLE = 4
-    }
-    public class HRG_DBConnection
-    {
-        public HRG_DBConnection(string connstr, HRG_SQLType type)
-        {
 
+    //虚拟工厂模式的数据库工厂类，根据传入的参数决定链接那个数据库
+    public class HRG_DBFactory : IDisposable
+    {
+        public HRG_DBFactory(IDbConnection  conn)
+        {
+            if (!(conn.State == ConnectionState.Closed || conn.State == ConnectionState.Broken))
+            {
+                throw new Exception(String.Format("Error , connection {0} is already opened !", conn.ConnectionString));
+            }
+            _conn = conn;
+            _conn.Open();
         }
 
-        public HRG_DBConnection(string configFile)
+        public HRG_DBFactory(string configFile)
         {
-
+            throw new Exception(String.Format("Error , HRG_DBConnection open by file {0} , function not finished yet!"));
         }
 
 
+        public HRG_IDataContext getDC()
+        {
+            HRG_IDataContext result;
+            if (_conn.GetType() == typeof(MySqlConnection))
+            {
+                result = new HRG_MysqlDataContext(_conn);
+            }
+            else if (_conn.GetType() == typeof(OleDbConnection))
+            {
+                result = new HRG_OleDBContext(_conn));
+            }
+            else
+            {
+                result = null;
+            }
+            return result;
+        }
+
+        public void Dispose()
+        {
+            if (_conn.State == ConnectionState.Open)
+                _conn.Close();
+            Console.WriteLine("conn closed!");
+        }
+
+        private IDbConnection _conn;
     }
 
 }
--- HRGLinqLibrary/HRG_DataContext.cs
+++ HRGLinqLibrary/HRG_DataContext.cs
@@ -14,13 +14,198 @@
 
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using HRG_BaseLibrary_2012;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data.OleDb;
 
-namespace HRGLinqLibrary
+namespace HRG_LinqLibrary
 {
-    class HRG_DataContext
+    ////查询条件
+    //enum SqlQueryType
+    //{
+    //    TSqlString = 1,
+    //    Process = 2
+    //}
+    ////数据查询信息类
+    //class queryObj
+    //{
+    //    SqlQueryType type;
+    //    string condition;
+    //    string argument;
+    //}
+    //数据操作类接口，暂时只有数据查询功能（2016.5.23）
+    public interface HRG_IDataContext
     {
+        string Query(string queryString);
+        string Query(object queryObj);
+        string QueryProc(string procName, string argumentJson);
+        string QueryProc(string procName);
     }
+
+    //mysql功能实现
+    #region mysql datacontext
+    public class HRG_MysqlDataContext : HRG_IDataContext
+    {
+        public HRG_MysqlDataContext(IDbConnection conn)
+        {
+            if (conn.State != ConnectionState.Open)
+            {
+                throw new Exception("ERROR, DataContext construct failed !");
+            }
+            _conn = (MySqlConnection)conn;
+        }
+
+        public string Query(string queryString)
+        {
+            throw new Exception("ERROR,Query not finished!");
+        }
+
+        public string Query(object queryObj)
+        {
+            throw new Exception("ERROR,Query not finished!");
+        }
+
+        public string QueryProc(string procName, string argumentJson)
+        {
+            throw new Exception("ERROR, QueryProc not finished!");
+        }
+
+        //无参形式的存储过程，一般来说应该是查询存储过程
+        public string QueryProc(string procName)
+        {
+            try
+            {
+                if (procName.IndexOf("SEARCH") < 0)
+                    throw new Exception("ERROR, QueryProc name invalid!");
+
+                #region 读取数据
+                //创建数据库命令  
+                MySqlCommand cmd = _conn.CreateCommand();
+                //创建查询语句  
+                cmd.CommandText = procName;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();  
+                //从数据库中读取数据流存入reader中  
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                //从reader中读取下一行数据,如果没有数据,reader.Read()返回flase  
+
+                if (!reader.HasRows)
+                    return ""; //一条数据都没有直接返回空字符串
+                DynDBHelper dynDb = new DynDBHelper();
+                while (reader.Read())
+                {
+                    List<FieldUnit> row = new List<FieldUnit>();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        FieldUnit unitTemp = new FieldUnit();
+                        unitTemp.FieldName = reader.GetName(i);
+                        unitTemp.FieldValue = reader[i].ToString();
+                        row.Add(unitTemp);
+                    }
+                    dynDb.addRow(row);
+                }
+
+                string json = CommonFunction.ToJson(dynDb.getDataList());
+                return json;
+                #endregion
+            }
+            catch
+            {
+                throw new Exception(String.Format("ERROR, Mysql DataContext error while do QueryProc: {0}", procName));
+            }
+            finally
+            {
+                //do nothing yet
+            }
+        }
+
+        private MySqlConnection _conn;
+    }
+    #endregion
+
+    //OleDb功能实现
+    #region OleDb connection
+    public class HRG_OleDBContext : HRG_IDataContext
+    {
+        public HRG_OleDBContext(IDbConnection conn)
+        {
+            if (conn.State != ConnectionState.Open)
+            {
+                throw new Exception("ERROR, DataContext construct failed !");
+            }
+            _conn = (OleDbConnection)conn;
+        }
+
+        //执行select查询语句
+        public string Query(string queryString)
+        {
+            
+            OleDbCommand command = _conn.CreateCommand();
+
+            command.CommandText = queryString;
+
+            OleDbDataReader reader = command.ExecuteReader();
+        }
+
+        //无参形式的存储过程，一般来说应该是查询存储过程
+    //    public string QueryProc(string procName)
+    //    {
+    //        try
+    //        {
+    //            if (procName.IndexOf("SEARCH") < 0)
+    //                throw new Exception("ERROR, QueryProc name invalid!");
+
+    //            #region 读取数据
+    //            //创建数据库命令  
+    //            MySqlCommand cmd = _conn.CreateCommand();
+    //            //创建查询语句  
+    //            cmd.CommandText = procName;
+    //            cmd.CommandType = CommandType.StoredProcedure;
+    //            cmd.Parameters.Clear();
+    //            //从数据库中读取数据流存入reader中  
+    //            MySqlDataReader reader = cmd.ExecuteReader();
+
+    //            //从reader中读取下一行数据,如果没有数据,reader.Read()返回flase  
+
+    //            if (!reader.HasRows)
+    //                return ""; //一条数据都没有直接返回空字符串
+    //            DynDBHelper dynDb = new DynDBHelper();
+    //            while (reader.Read())
+    //            {
+    //                List<FieldUnit> row = new List<FieldUnit>();
+    //                for (int i = 0; i < reader.FieldCount; i++)
+    //                {
+    //                    FieldUnit unitTemp = new FieldUnit();
+    //                    unitTemp.FieldName = reader.GetName(i);
+    //                    unitTemp.FieldValue = reader[i].ToString();
+    //                    row.Add(unitTemp);
+    //                }
+    //                dynDb.addRow(row);
+    //            }
+
+    //            string json = CommonFunction.ToJson(dynDb.getDataList());
+    //            return json;
+    //            #endregion
+    //        }
+    //        catch
+    //        {
+    //            throw new Exception(String.Format("ERROR, Mysql DataContext error while do QueryProc: {0}", procName));
+    //        }
+    //        finally
+    //        {
+    //            //do nothing yet
+    //        }
+    //    }
+
+        private OleDbConnection _conn;
+    }
+    #endregion
+
 }
